c4b3e77922635a1cb96b71af222f3b9a363836d7
Azaryan1 <h_azaryan@hotmail.com>
2017-09-10 00:33:32 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,11 @@
+
+const num1 = 3;
+const num2 = 5;
+console.log(num1+num2);
+console.log(num1*num2);
+if(num1+num2===8){
+console.log("What did the blonde say when she found out she was pregnant? I wonder if it's mine")
+}
+else{
+console.log("Why did the blonde put lipstick on her forehead? Because she wanted to make up her mind")
+}
